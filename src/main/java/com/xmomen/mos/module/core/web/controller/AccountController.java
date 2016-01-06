a01737d1a401a8dbc68a393fcc45915f999e9938
Jeng <jeng.tam@udfex.com>
2016-01-07 01:11:02 +0800

    账户管理设计及开发

---
+++ src/main/java/com/xmomen/mos/module/core/web/controller/AccountController.java
@@ -0,0 +1,36 @@
+package com.xmomen.mos.module.core.web.controller;
+
+import com.xmomen.mos.module.core.web.controller.dto.AccountSettingDto;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.context.annotation.Role;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Jeng on 2016/1/5.
+ */
+@RestController
+public class AccountController {
+
+    @RequestMapping(value = "/account/setting", method = RequestMethod.GET)
+    public AccountSettingDto accountSetting(){
+        AccountSettingDto accountSettingDto = new AccountSettingDto();
+        Subject subject = SecurityUtils.getSubject();
+        if(subject == null){
+            return null;
+        }
+        accountSettingDto.setUsername(subject.getPrincipal().toString());
+        List roles = new ArrayList<AccountSettingDto.Role>();
+        Set set = subject.getPrincipals().getRealmNames();
+        //roles.add(role);
+        //accountSettingDto.setRoles(roles);
+        return accountSettingDto;
+    }
+
+}
